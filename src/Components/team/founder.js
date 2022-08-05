54c6c7c20610ef8b51474ebc34326a51799f337e
SAKHIBSHAIKH786 <85670761+SAKHIBSHAIKH786@users.noreply.github.com>
2022-08-05 22:00:48 +0530

    Update founder.js

--- src/Components/team/founder.js
+++ src/Components/team/founder.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./founder.css";
 // Images
-import arnav from "../../assets/team-cores/arnav.webp";
+import Arnav from "../../assets/team-cores/arnav.webp";
 const Founder = () => {
  return (
  <>
@@ -11,7 +11,7 @@ const Founder = () => {
  <div className="founder-image-container">
  <img
  className="founder-image"
- src={arnav}
+ src={Arnav}
  alt="founder"
  />
  </div>
